897dfff395de619acc5e525e4af9123192ea528b
Hữu Chu <80379069+DenoLab@users.noreply.github.com>
2025-03-10 18:18:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ import React, { useState } from 'react';
 
 function App() {
   // State lưu địa chỉ IP của điện thoại
-  const [ipAddress, setIpAddress] = useState('https://10.147.18.207:8080/');
+  const [ipAddress, setIpAddress] = useState('http://10.147.18.207:8080/');
 
   // Tạo URL để lấy video
   const videoUrl = ipAddress ? `${ipAddress}/video` : '';
